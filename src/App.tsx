e4fb4b7a1033d7ebb9daf8c798dff7f80554c48c
Mirailisc <mirailisclm@gmail.com>
2022-01-12 21:59:39 +0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { app } from './service/Firebase'
+import { getDatabase, ref, onValue, query, set } from 'firebase/database'
+import './App.scss'
+
+const database = getDatabase(app)
+const dbRef = ref(database, 'status')
 
 function App() {
+  const [status, setStatus] = useState<string>('Standby')
+
+  const handleWorking = async () => {
+    await set(dbRef, 'Working')
+  }
+
+  const handleStandby = async () => {
+    await set(dbRef, 'Standby')
+  }
+
+  useEffect(() => {
+    onValue(query(dbRef), (snapshot) => {
+      setStatus(snapshot.val())
+    })
+  }, [])
+
+  const handleNextStage = async () => {
+    await set(dbRef, 'Standby')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">Arduino Project</h1>
+      <div className="container">
+        <h3>สถานะอุปกรณ์</h3>
+        <div className="row">
+          {status === 'Standby' ? (
+            <div className="switcher-standby" onClick={() => handleWorking()}></div>
+          ) : (
+            <div className="switcher" onClick={() => handleStandby()}></div>
+          )}
+          <div className="statusPanel">
+            <p className={status}>
+              <b>สถานะ: </b> {status}
+            </p>
+            <p className={status === 'Waiting' ? 'waitingStatus' : 'notWaitingStatus'}>รอการรดน้ำ</p>
+          </div>
+          {status === 'Waiting' ? (
+            <div className="buttonGroup">
+              <button className="done-water" onClick={() => handleNextStage()}>
+                รดน้ำ
+              </button>
+              <button className="skip-water" onClick={() => handleNextStage()}>
+                ข้ามการรดน้ำ
+              </button>
+            </div>
+          ) : null}
+        </div>
+        {status === 'Waiting' ? (
+          <div className="buttonGroup-responsive">
+            <button className="done-water" onClick={() => handleNextStage()}>
+              รดน้ำแล้ว
+            </button>
+            <button className="skip-water" onClick={() => handleNextStage()}>
+              ข้ามการรดน้ำ
+            </button>
+          </div>
+        ) : null}
+        <h3>ข้อมูล</h3>
+        <div className="informationPanel">
+          <h3>สถานะทั้งหมด</h3>
+          <ul>
+            <li>
+              <span className="standby">Standby: </span>ระบบกำลังรอการดำเนินการ
+            </li>
+            <li>
+              <span className="working">Working: </span>มอเตอร์กำลังทำงานอยู่
+            </li>
+            <li>
+              <span className="waiting">Waiting: </span>ระบบกำลังรอการรดน้ำ
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
